93d148edd45a06f254caf82dbf986865a163b96e
Erick Velez <erickvelez7@gmail.com>
2024-09-27 20:19:28 -0700

    write locks to lock.json
    also reformat everything to conform to rustiness

--- src/file_manager/mod.rs
+++ src/file_manager/mod.rs
@@ -1,14 +1,58 @@
-  use std::fs::OpenOptions;
-  use std::io::{ BufReader, BufRead, Result };
+use serde::{Deserialize, Serialize};
+use std::fs::OpenOptions;
+use std::io::{BufRead, BufReader, BufWriter, Result};
 
-  pub fn lock(file_name : &String, begin : &usize, end : &usize) -> Result<bool>  {
-    let file = OpenOptions::new().read(true).open(file_name)?;
-    let line_count = BufReader::new(file).lines().count();
-    
-    if line_count < *end {
-      println!("Requested lock goes beyond file's line count");
-      return Ok(false)
+#[derive(Serialize, Deserialize)]
+struct Lock {
+    file_name: String,
+    begin: usize,
+    end: usize,
+}
+
+pub fn lock(file_name: String, begin: usize, end: usize) -> Result<bool> {
+    {
+        let file = OpenOptions::new()
+            .read(true)
+            .open(file_name.clone())
+            .expect("File couldn't open!");
+        let line_count = BufReader::new(file).lines().count();
+
+        if line_count < end {
+            return Ok(false);
+        }
     }
-    
+
+    let file = OpenOptions::new()
+        .read(true)
+        //.write(true)
+        .open(".janus/locks.json")
+        .expect("Could not open locks.json");
+    //let file_write_copy = file.try_clone().expect("Could not copy lock.json");
+    let mut locks: Vec<Lock> =
+        serde_json::from_reader(BufReader::new(file)).expect("Could not read locks.json.");
+
+    // FIXME: Check if the file is being tracked
+    for lock in &locks {
+        if lock.file_name == *file_name && (lock.begin >= begin || lock.end <= end) {
+            // FIXME: A user should only place one lock per file
+            println!("A lock already exists for this file!");
+            return Ok(false);
+        }
+    }
+
+    let file_write_copy = OpenOptions::new()
+        .write(true)
+        .truncate(true)
+        .open(".janus/locks.json")
+        .expect("Could not open locks.json to write");
+    let new_lock = Lock {
+        file_name: file_name,
+        begin: begin,
+        end: end,
+    };
+    locks.push(new_lock);
+    serde_json::to_writer(BufWriter::new(file_write_copy), &locks)
+        .expect("Failed to write lock to file.");
+
     Ok(true)
-  }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,59 @@
 use clap::{Parser, Subcommand};
-use std::fs::{ self, OpenOptions };
+use std::{
+    fs::{self},
+    io::Write,
+};
 pub mod file_manager;
 
 #[derive(Debug, Parser)]
-#[clap(name="janus", version)]
+#[clap(name = "janus", version)]
 struct Args {
-  #[clap(subcommand)]
-  command: Commands,
+    #[clap(subcommand)]
+    command: Commands,
 }
 
 #[derive(Debug, Subcommand)]
 enum Commands {
-  /// Initialize a janus repo
-  Init,
-  /// Lock a file
-  Lock {
-    /// The name of the file to lock
-    file_name: String,
-    /// The line to begin the lock
-    begin: usize,
-    /// The line to end the lock
-    end: usize
-  },
+    /// Initialize a janus repo
+    Init,
+    /// Lock a file
+    Lock {
+        /// The name of the file to lock
+        file_name: String,
+        /// The line to begin the lock
+        begin: usize,
+        /// The line to end the lock
+        end: usize,
+    },
 }
 
 fn main() {
-  let args = Args::parse();
-  
-  match &args.command {
-    Commands::Lock { file_name, begin, end } => {
-      println!("Locking {:?} from lines {:?} to {:?}", file_name, begin, end);
-      file_manager::lock(file_name, begin, end);
+    let args = Args::parse();
+
+    match &args.command {
+        Commands::Lock {
+            file_name,
+            begin,
+            end,
+        } => {
+            println!(
+                "Locking {:?} from lines {:?} to {:?}",
+                file_name, begin, end
+            );
+            file_manager::lock(file_name.to_string(), *begin, *end)
+                .expect("Could not write lock to file");
+        }
+
+        Commands::Init => {
+            println!("Initializing a janus repo here");
+            fs::create_dir_all(".janus/refs").expect("Could not create refs directory.");
+            fs::create_dir(".janus/objects").expect("Could not create objects directory.");
+
+            let mut locks =
+                fs::File::create(".janus/locks.json").expect("Could not create locks.json");
+            locks
+                .write_all(b"[]\n")
+                .expect("Could not write to locks file");
+        }
     }
-    
-    Commands::Init => {
-      println!("Initializing a janus repo here");
-      match fs::create_dir_all(".janus/refs") {
-        Ok(_res) => println!("Directory created"),
-        Err(e) => println!("Could not create dir\n {:?}", e)
-      }
-      
-      match fs::create_dir(".janus/objects") {
-        Ok(_res) => println!("Directory created"),
-        Err(e) => println!("Could not create dir\n {:?}", e)
-      }
-      
-      match fs::File::create(".janus/locks.json") {
-        Ok(_res) => println!("Created locks file"),
-        Err(e) => println!("Could not create dir\n {:?}", e)
-      }
-    }
-  }
 }
